01922ac59412624f6061f07616b8917da71a6547
Pablo <pablo-nahuel@hotmail.com>
2024-01-16 15:11:06 -0300

    se agregó boton de descarga app

--- script.js
+++ script.js
@@ -37,3 +37,37 @@ changeThemeColor();
 // Añadir un evento que escuche los cambios de conexión
 window.addEventListener("online", changeThemeColor);
 window.addEventListener("offline", changeThemeColor);
+
+
+// Para boton de instalación de APP
+// Obtiene el botón de instalación
+const installButton = document.querySelector("#install");
+
+// Declara una variable para almacenar el evento beforeinstallprompt
+let installPrompt = null;
+
+// Registra un listener para el evento beforeinstallprompt
+window.addEventListener("beforeinstallprompt", (event) => {
+  // Evita que el navegador muestre su propia UI de instalación
+  event.preventDefault();
+  // Guarda el evento para usarlo más tarde
+  installPrompt = event;
+  // Muestra el botón de instalación
+  installButton.removeAttribute("hidden");
+});
+
+// Registra un listener para el evento click del botón de instalación
+installButton.addEventListener("click", async () => {
+  // Verifica que el evento beforeinstallprompt exista
+  if (!installPrompt) {
+    return;
+  }
+  // Muestra el diálogo de instalación
+  const result = await installPrompt.prompt();
+  // Registra el resultado del diálogo de instalación
+  console.log(`El resultado del diálogo de instalación fue: ${result.outcome}`);
+  // Borra el evento beforeinstallprompt
+  installPrompt = null;
+  // Oculta el botón de instalación
+  installButton.setAttribute("hidden", "");
+});
